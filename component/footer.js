d4bf620c832f73813e3d49222d876821b5740e68
waltor <longmengua@gmail.com>
2020-03-11 11:40:17 +0800

    extract footer

--- component/footer.js
+++ component/footer.js
@@ -1,18 +1,34 @@
 Vue.component('v-footer', {
     props: ['selected'],
-    data: {
-        en: {
-            footer: [
-                'This website is refer to the w3school template.(Using Vue.js to do i18n.)',
-                'Find me on social media.',
-            ],
-        },
-        zh_tw: {
-            footer: [
-                '這個網站是參考 w3school 範本. (使用 Vue.js 實作多國語系.）',
-                '以下是我的社交軟體',
+    data: function () {
+        return {
+            i18ns: {
+                en: {
+                    footer: [
+                        'Find me on social media.',
+                    ],
+                },
+                zh_tw: {
+                    footer: [
+                        '以下是我的社交軟體',
+                    ],
+                }
+            },
+            href: [
+                "https://www.facebook.com/longmengua",
+                "https://www.instagram.com/waltor1991/?r=nametag&fbclid=IwAR3DN39lbUdvQ5-aTia9AcAiJMHUrARZv3qewDOvNpCjiWRGRAgSOBPfo8M",
+                "https://www.linkedin.com/in/鴻文-黃-17b61b17b",
+                "https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_cv&stacked=h",
+                "https://line.me/ti/p/9UMBNQ5K5p",
             ],
         }
     },
-    template: '<p>{{[selected].footer}}</p>',
+    template: '<footer style="padding:20px 0px;" class="w3-container w3-teal w3-center w3-margin-top">'
+    + "<p>{{i18ns[selected]['footer'][0]}}</p>"
+    + '<a href={{href[0]}}><i style="font-size: 40px;margin-right: 20px;" class="fa fa-facebook-official w3-hover-opacity"></i></a>'
+    + '<a href={{href[1]}}><i style="font-size: 40px;margin-right: 20px;" class="fa fa-instagram w3-hover-opacity"></i></a>'
+    + '<a href={{href[2]}}><i style="font-size: 40px;margin-right: 20px;" class="fa fa-linkedin w3-hover-opacity"></i></a>'
+    + '<a href={{href[3]}}><i style="font-size: 40px;margin-right: 20px;" class="fa fa-line w3-hover-opacity"></i></a>'
+    + '<br/>'
+    + '</footer>'
 })
