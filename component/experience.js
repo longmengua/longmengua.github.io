f0a102cf92cf8a85cb494ba69b5bc1615a244a7b
黃鴻文 <longmengua@gmail.com>
2020-04-16 05:37:19 +0800

    updaterd

--- component/experience.js
+++ component/experience.js
@@ -12,16 +12,16 @@ Vue.component('v-experience', {
                 en: {
                     workExperienceTitle : 'Work Experience',
                     workExperience: [
-                        {title:'Software Engineer / KENCONTS LTD.', date:'2019.08 - Now',content:'On-site  working  in  H2UClub  as  Full-stack  and  Flutter  programmer.(Full-stack + App programmer with java, flutter, swift, android.)'},
-                        {title:'Software Engineer / WISTRON ITS', date:'2019.03 - 2019.08',content:'1. On-site working in Fubon Bank. Missions are consist of discussing the business of documents is logical with BA by phone call, and working with programmers of other companies. 2. On-site working in Cathay Bank. Working in Front-End with react.js for maintaining the existing project and angular 8 for converting  react.js to angular 8. ( Full-stack programmer with java, react, angular 8. )'},
-                        {title:'Software Engineer / LISCO Global SOLUTION LTD.', date:'2018.02 - 2019.01',content:'Develop  a  vendor  management  system,  work  as  a  full  stack,  my  jobs  contain  maintain  the  existing  project,  and  try  to  find  out  something  can  make  it  better. ( Full-stack programmer with java, angularJS. )'},
-                        {title:'Software Assistant Engineer / RSL LTD.', date:'2016.10 - 2017.03',content:'Maintaining  existing  projects,  using  the  same  template  to  develop  a  product,  cross-platform,  like  the  window,  Ubuntu. The  product  is  developed  in  traditional  c,  using  points,  struct,  memory  operation. ( Traditional C programmer. )'},
+                        {title:'Software Engineer / KENCONTS LTD.', date:'2019.08 - Now',content:'On-site working in H2UClub as Full-stack and Flutter programmer. Have two experiences of developing apps, one is co-working with three co-workers, another one is developing individually, translating the existing android app to a cross-platform app by flutter.'},
+                       {title:'Software Engineer / WISTRON ITS', date:'2019.03 - 2019.08',content:'1. On-site working in Fubon Bank. Missions are consist of discussing the business documents is logical with BA, working with programmers of other companies. 2. On-site working as a Front-End programmer in Cathay Bank. Using react.js for maintaining the existing project, angular 8 for developing an app by translating react js to angular 8.'},
+                        {title:'Software Engineer / LISCO Global SOLUTION LTD.', date:'2018.02 - 2019.01',content:'Develop a vendor management system, work as a full stack, my jobs contain maintain the existing project, and try to find out something can make it better. ( Full-stack programmer with java, angularJS. )'},
+                        {title:'Software Assistant Engineer / RSL LTD.', date:'2016.10 - 2017.03',content:'Maintaining existing projects, using the same template to develop a product, cross-platform, like the window, Ubuntu. The product is developed in traditional c, using points, struct, memory operation. (Traditional C programmer.)'},
                     ],
                 },
                 zh_tw: {
                     workExperienceTitle : '經歷',
                     workExperience: [
-                        {title:'軟體工程師 / 宏益科技服務', date:'2019／08~Now',content:'駐點在鴻海旗下(永悅健康)，擔任全端以及手機軟題開發 (要自己寫Restful API及App畫面，使用 ngrok 做Debug。)，與其他工程師一起開發Hi365 (已上架），以及獨立開發Flutter App ( 將已存在的Android app，使用Flutter技術將其翻譯成iOS上的App )。'},
+                        {title:'軟體工程師 / 宏益科技服務', date:'2019／08~Now',content:'駐點在鴻海旗下(永悅健康)，擔任全端以及手機軟體開發。開發兩個ＡＰＰ，一個是hi365，另一個是獨立開發，將既有的Android app，使用Flutter技術將其翻譯成誇平台App。'},
                         {title:'軟體工程師 / 緯創軟體', date:'2019／03～2019／08',content:'1. 駐點富邦銀行，擔任Java工程師，工作內容是和BA討論規格書是否有邏輯上的衝突以及開發軟體。2. 駐點國泰銀行，擔任前端工程師，維護既有React專案以及將此案子轉換成Angular 8。'},
                         {title:'軟體工程師 / 仕傑科技', date:'2018／02～2019／01',content:'全端開工程師，使用AngularJS、Java，框架有Spring、Spring boot、Spring boot、Spring security、Spring Data、Elastic Search、Liquibase、AngularJS、Jhipster、Gulp、Bower，使用過的標準有Oauth2，資料庫MYSQL。Project是像似104人力銀行。前端幾乎都使用Javascript、CSS3，不熟Bootstrap、Jquery，也比較偏好寫Javascript、CSS3。'},
                         {title:'軟體助理工程師 / 宏誌科技', date:'2016／10～2017／03',content:'維護既有POS系統，開發新功能，跨平台開發(Ubuntu, Window.)，使用C語言，開發工具CBC和Qt。'},
