bcb5ae8925397956dec866bc111287ba9a90af40
黃鴻文 <longmengua@gmail.com>
2020-04-16 01:47:45 +0800

    updated

--- page/home.js
+++ page/home.js
@@ -9,22 +9,22 @@ Vue.component('v-home', {
     <div id="home" class="w3-row-padding main">
         <div class="w3-third left-side">
             <div style="text-align:center;">
-                <v-label-left v-bind:selected="selected" v-on:out="label-option = $event"></v-label-left>
+                <v-label-left v-bind:selected="selected" v-on:out="labelOption = $event"></v-label-left>
             </div>
             <div class="w3-white w3-text-grey w3-card-4">
-                <div v-show="label-option == 0" class="w3-display-container">
+                <div v-show="labelOption == 0" class="w3-display-container">
                     <v-avatar v-bind:selected="selected"></v-avatar>
                 </div>
-                <div v-show="label-option == 0" class="w3-display-container">
+                <div v-show="labelOption == 0" class="w3-display-container">
                     <v-social v-bind:selected="selected" style="background-color:white;color:#009688!important;"></v-social>
                     <v-profile v-bind:selected="selected"></v-profile>
                 </div>
                 <hr/>
-                <div v-show="label-option == 0" class="w3-container">
+                <div v-show="labelOption == 0" class="w3-container">
                     <v-language v-bind:selected="selected"></v-language>
                 </div>
                 <br/>
-                <div v-show="label-option == 1" class="w3-container">
+                <div v-show="labelOption == 1" class="w3-container">
                     <v-skill v-bind:selected="selected"></v-skill>
                 </div>
                 <br/>
